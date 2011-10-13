296dc9702acbe54059f2ffbe7d6908d9eb4d4242
GRPL <bott@grpl.org>
2011-10-13 17:32:16 -0400

    expose the block list when first pulling up a patron

--- web/xul/rel_2_1/server/patron/display.js
+++ web/xul/rel_2_1/server/patron/display.js
@@ -961,7 +961,10 @@ obj.summary_window.g.summary.controller.render('patron_checkouts');
                             dl_flag_opened = true;
                         }
                         msg += '<dt>';
-                        msg += obj.OpenILS.data.hash.aou[ penalties[i].org_unit() ].shortname() + ' : ' + penalties[i].standing_penalty().label() + '<br/>';
+                        var blist='';
+                        if (penalties[i].standing_penalty().block_list())
+                                blist = ' (BLOCKED: '+penalties[i].standing_penalty().block_list()+')';
+                        msg += obj.OpenILS.data.hash.aou[ penalties[i].org_unit() ].shortname() + ' : ' + penalties[i].standing_penalty().label() + blist + '<br/>';
                         msg += '</dt><dd>';
                         msg += (penalties[i].note())?penalties[i].note():'';
                         msg += '</dd>';
