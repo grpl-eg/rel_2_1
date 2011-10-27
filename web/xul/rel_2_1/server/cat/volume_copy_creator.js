d2086b7b59907d4d177438ed996c466460fa861b
GRPL <bott@grpl.org>
2011-10-27 17:24:46 -0400

    default cursor to callnumber field for OE as well

--- web/xul/rel_2_1/server/cat/volume_copy_creator.js
+++ web/xul/rel_2_1/server/cat/volume_copy_creator.js
@@ -739,7 +739,7 @@ g.render_barcode_entry = function(node,callnumber_composite_key,count,ou_id) {
                 if (! g.first_focus) { g.first_focus = tb; }
             }
             if (g.use_defaults && ! g.first_focus) {
-if (ses('ws_ou') < 9 || ses('ws_ou') > 18) {
+if ( (ses('ws_ou') < 9 || ses('ws_ou') > 17) && (ses('ws_ou') < 30 || ses('ws_ou') > 33) ) {
                 g.first_focus = tb;
                 tb.focus();
 }
